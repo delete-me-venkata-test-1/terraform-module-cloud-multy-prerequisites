ea677ee1614557cbb6f8516fdbb407e3e7f62207
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-05-13 03:26:44 +0000

    fix: remove verions tags

--- providers.tf
+++ providers.tf
@@ -2,15 +2,12 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "4.5.0"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "4.67.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.5.1"
     }
   }
 }
