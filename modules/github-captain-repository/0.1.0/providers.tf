5d9516b8621102b3c0c3bcd238272d345be5bc47
Venkata Mutyala <venkata.mutyala@glueops.dev>
2023-06-13 00:52:15 +0000

    feat: migrate to terraform-registry-proxy.gpkg.io

--- modules/github-captain-repository/0.1.0/providers.tf
+++ modules/github-captain-repository/0.1.0/providers.tf
@@ -1,8 +1,7 @@
 terraform {
   required_providers {
     github = {
-      source  = "integrations/github"
-      version = "5.25.0"
+      source  = "terraform-registry-proxy.gpkg.io/integrations/github"
     }
   }
 }
