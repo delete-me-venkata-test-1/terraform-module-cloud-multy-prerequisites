4aec6f30aa1622db0c712d212a0e4b7e82b58028
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-11-06 18:46:00 +0000

    feat(opsgenie_schedule_rotation): move rotation start to daytime PST, use utc timezone

---
+++ modules/opsgenie/0.2.0/main.tf
@@ -0,0 +1,99 @@
+locals {
+  users_set                = toset(var.users)
+  cluster_environments_set = toset(var.cluster_environments)
+  time_zone                = "UTC"
+  rotation_start_date      = "2023-03-14T20:00:00Z" # Tuesday at 12pm PST (with daylight savings) - Set the start date and time for the rotation
+}
+
+data "opsgenie_user" "users" {
+  for_each = local.users_set
+  username = each.key
+}
+
+resource "opsgenie_team" "teams" {
+  for_each = local.cluster_environments_set
+
+  name                     = "${var.tenant_key}-${each.value}-team"
+  description              = "This is for company ${var.tenant_key} in the ${each.value} environment"
+  delete_default_resources = true
+
+  dynamic "member" {
+    for_each = data.opsgenie_user.users
+
+    content {
+      id   = member.value.id
+      role = "user"
+    }
+  }
+}
+
+resource "opsgenie_team_routing_rule" "routing_rules" {
+  for_each = local.cluster_environments_set
+
+  name     = "${var.tenant_key}-${each.value}-routing-rule"
+  team_id  = opsgenie_team.teams[each.key].id
+  order    = 0
+  timezone = local.time_zone
+  criteria {
+    type = "match-all"
+  }
+  notify {
+    name = opsgenie_schedule.schedules[each.key].name
+    type = "schedule"
+  }
+
+}
+
+resource "opsgenie_schedule" "schedules" {
+  for_each = local.cluster_environments_set
+
+  name          = "${var.tenant_key}-${each.value}-schedule"
+  description   = "This is for company ${var.tenant_key} in the ${each.value} environment"
+  timezone      = local.time_zone
+  owner_team_id = opsgenie_team.teams[each.key].id
+  enabled       = true
+}
+
+resource "opsgenie_schedule_rotation" "rotations" {
+  for_each = local.cluster_environments_set
+
+  schedule_id = opsgenie_schedule.schedules[each.key].id
+  name        = "${var.tenant_key}-${each.value}-rotation"
+
+  dynamic "participant" {
+    for_each = data.opsgenie_user.users
+
+    content {
+      type = "user"
+      id   = participant.value.id
+    }
+  }
+
+  start_date = local.rotation_start_date
+  type       = "weekly"
+  length     = 1
+}
+
+resource "opsgenie_api_integration" "prometheus" {
+  for_each = local.cluster_environments_set
+  name     = "${var.tenant_key}-${each.value}-prometheus-api-int"
+  type     = "API"
+
+  responders {
+    type = "team"
+    id   = opsgenie_team.teams[each.key].id
+  }
+
+  enabled                        = true
+  allow_write_access             = true
+  ignore_responders_from_payload = true
+  suppress_notifications         = false
+  owner_team_id                  = opsgenie_team.teams[each.key].id
+}
+
+output "opsgenie_prometheus_api_keys" {
+  value = {
+    for env_key, env_value in local.cluster_environments_set : env_key => opsgenie_api_integration.prometheus[env_key].api_key
+  }
+  description = "A map of the Opsgenie API keys for the Prometheus integrations."
+}
---
+++ modules/opsgenie/0.2.0/providers.tf
@@ -0,0 +1,7 @@
+terraform {
+  required_providers {
+    opsgenie = {
+      source = "opsgenie/opsgenie"
+    }
+  }
+}
