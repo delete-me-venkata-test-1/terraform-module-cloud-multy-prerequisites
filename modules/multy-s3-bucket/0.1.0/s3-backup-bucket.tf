4b3454245462a3705719df0c21aba01dcd0bf140
Venkata Mutyala <venkata@venkatamutyala.com>
2024-04-11 14:46:32 -0700

    fix: s3 bucket dependencies (replica)

--- modules/multy-s3-bucket/0.1.0/s3-backup-bucket.tf
+++ modules/multy-s3-bucket/0.1.0/s3-backup-bucket.tf
@@ -50,4 +50,8 @@ resource "aws_s3_bucket_public_access_block" "replica" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+
+  depends_on = [
+    aws_s3_bucket_lifecycle_configuration.replica
+  ]
 }
