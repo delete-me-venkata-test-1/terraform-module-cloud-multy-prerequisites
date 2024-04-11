44ed94b09ab9cad5d28694574ae948c1a643cbc7
Venkata Mutyala <venkata@venkatamutyala.com>
2024-04-11 14:46:12 -0700

    fix: s3 bucket dependencies (primary bucket)

--- modules/multy-s3-bucket/0.1.0/s3-primary-bucket.tf
+++ modules/multy-s3-bucket/0.1.0/s3-primary-bucket.tf
@@ -54,4 +54,8 @@ resource "aws_s3_bucket_public_access_block" "primary" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+
+  depends_on = [
+    aws_s3_bucket_lifecycle_configuration.primary
+  ]
 }
