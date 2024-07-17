efd81716ffd7b99019371299596ddcf53acdfaa1
Venkat <venkata@venkatamutyala.com>
2024-07-16 22:58:15 -0700

    feat: update versions (#200)
    * feat: update versions
    
    * docs: automated update of terraform docs
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- modules/tenant-readme/0.1.0/readme.tf
+++ modules/tenant-readme/0.1.0/readme.tf
@@ -39,10 +39,10 @@ data "local_file" "readme" {
 }
 
 locals {
-  codespace_version         = "v0.44.0"
+  codespace_version         = "v0.45.0"
   argocd_crd_version        = var.argocd_app_version
   argocd_helm_chart_version = "5.54.0"
-  glueops_platform_version  = "v0.45.0" # this also needs to be updated in the module.glueops_platform_helm_values // generate-helm-values.tf
+  glueops_platform_version  = "v0.46.0-rc1" # this also needs to be updated in the module.glueops_platform_helm_values // generate-helm-values.tf
   tools_version             = "v0.10.1"
 }
 
