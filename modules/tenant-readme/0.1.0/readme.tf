794405d1a752d7985059ded5256843db8794b074
Venkat <venkata@venkatamutyala.com>
2024-08-06 12:52:43 -0700

    fix: platform v0.47.0 -> v0.47.1 fixing argocd alert for new redis job (#209)
    * fix: platform v0.47.0 -> v0.47.1 fixing argocd alert for new redis job
    
    * docs: automated update of terraform docs
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- modules/tenant-readme/0.1.0/readme.tf
+++ modules/tenant-readme/0.1.0/readme.tf
@@ -42,7 +42,7 @@ locals {
   codespace_version         = "v0.47.0"
   argocd_crd_version        = var.argocd_app_version
   argocd_helm_chart_version = "7.4.0"
-  glueops_platform_version  = "v0.47.0" # this also needs to be updated in the module.glueops_platform_helm_values // generate-helm-values.tf
+  glueops_platform_version  = "v0.47.1" # this also needs to be updated in the module.glueops_platform_helm_values // generate-helm-values.tf
   tools_version             = "v0.10.2"
 }
 
