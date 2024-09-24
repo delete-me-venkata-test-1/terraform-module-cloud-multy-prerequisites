8bce6a4752a8cebf451483d529a790e4c9e55a52
Venkat <venkata@venkatamutyala.com>
2024-09-24 14:23:26 -0600

    major: removing waf/webacls apps (#221)
    * major: removing waf/webacls apps
    chore: update codespace
    
    * docs: automated update of terraform docs
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- modules/tenant-readme/0.1.0/readme.tf
+++ modules/tenant-readme/0.1.0/readme.tf
@@ -39,10 +39,10 @@ data "local_file" "readme" {
 }
 
 locals {
-  codespace_version         = "v0.48.0"
+  codespace_version         = "v0.49.0"
   argocd_crd_version        = var.argocd_app_version
   argocd_helm_chart_version = "7.5.2"
-  glueops_platform_version  = "v0.49.0" # this also needs to be updated in the module.glueops_platform_helm_values // generate-helm-values.tf
+  glueops_platform_version  = "v0.50.0" # this also needs to be updated in the module.glueops_platform_helm_values // generate-helm-values.tf
   tools_version             = "v0.11.1"
 }
 
