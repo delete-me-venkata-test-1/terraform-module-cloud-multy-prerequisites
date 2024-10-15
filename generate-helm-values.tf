4d31191f446363d473d18817d8676d547c0c3d82
Prasanth Viswanadham <prasath@citoit.io>
2024-10-15 12:58:18 -0400

    added the thanos and tempo to helm values

--- generate-helm-values.tf
+++ generate-helm-values.tf
@@ -55,10 +55,12 @@ module "glueops_platform_helm_values" {
   dex_pomerium_client_secret                 = random_password.dex_pomerium_client_secret[each.value.environment_name].result
   vault_aws_access_key                       = aws_iam_access_key.vault_s3[each.value.environment_name].id
   vault_aws_secret_key                       = aws_iam_access_key.vault_s3[each.value.environment_name].secret
-  #loki_aws_access_key                        = ""
-  #loki_aws_secret_key                        = ""
   loki_aws_access_key                        = aws_iam_access_key.loki_s3["loki.${each.value.environment_name}.${var.tenant_key}.${data.aws_route53_zone.management_tenant_dns.name}"].id
   loki_aws_secret_key                        = aws_iam_access_key.loki_s3["loki.${each.value.environment_name}.${var.tenant_key}.${data.aws_route53_zone.management_tenant_dns.name}"].secret
+  thanos_aws_access_key                      = aws_iam_access_key.loki_s3["thanos.${each.value.environment_name}.${var.tenant_key}.${data.aws_route53_zone.management_tenant_dns.name}"].id
+  thanos_aws_secret_key                      = aws_iam_access_key.loki_s3["thanos.${each.value.environment_name}.${var.tenant_key}.${data.aws_route53_zone.management_tenant_dns.name}"].secret
+  tempo_aws_access_key                       = aws_iam_access_key.loki_s3["tempo.${each.value.environment_name}.${var.tenant_key}.${data.aws_route53_zone.management_tenant_dns.name}"].id
+  tempo_aws_secret_key                       = aws_iam_access_key.loki_s3["tempo.${each.value.environment_name}.${var.tenant_key}.${data.aws_route53_zone.management_tenant_dns.name}"].secret
   certmanager_aws_access_key                 = aws_iam_access_key.certmanager[each.value.environment_name].id
   certmanager_aws_secret_key                 = aws_iam_access_key.certmanager[each.value.environment_name].secret
   externaldns_aws_access_key                 = aws_iam_access_key.externaldns[each.value.environment_name].id
--- save-credentials-to-s3.tf
+++ save-credentials-to-s3.tf
@@ -8,6 +8,8 @@ resource "aws_s3_object" "combined_outputs" {
     certmanager_credentials = { (aws_route53_zone.clusters[each.value].name) = aws_iam_access_key.certmanager[each.value] },
     externaldns_credentials = { (aws_route53_zone.clusters[each.value].name) = aws_iam_access_key.externaldns[each.value] },
     loki_credentials        = { (aws_route53_zone.clusters[each.value].name) = aws_iam_access_key.loki_s3["loki.${each.value}.${var.tenant_key}.${data.aws_route53_zone.management_tenant_dns.name}"] },
+    tempo_credentials       = { (aws_route53_zone.clusters[each.value].name) = aws_iam_access_key.loki_s3["tempo.${each.value}.${var.tenant_key}.${data.aws_route53_zone.management_tenant_dns.name}"] },
+    thanos_credentials      = { (aws_route53_zone.clusters[each.value].name) = aws_iam_access_key.loki_s3["thanos.${each.value}.${var.tenant_key}.${data.aws_route53_zone.management_tenant_dns.name}"] },
     opsgenie_credentials    = lookup(module.opsgenie_teams.opsgenie_prometheus_api_keys, split(".", each.value)[0], null),
     vault_credentials       = { (aws_route53_zone.clusters[each.value].name) = aws_iam_access_key.vault_s3[each.value] },
   })
